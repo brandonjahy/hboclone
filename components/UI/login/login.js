b0dc18bb5a2c9480f019009e04ac4f9e85012b57
Brandonjahy <b.johnss97@gmail.com>
2021-08-03 19:19:13 -0700

    abilty to save users

--- components/UI/login/login.js
+++ components/UI/login/login.js
@@ -1,9 +1,40 @@
-import Head from 'next/head'
-import { useStateContext } from '../../HBOProvider'
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { useStateContext } from '../../HBOProvider';
+import { useRouter } from 'next/router';
+import ls from 'local-storage';
 
 
 const Login = () => {
   const globalState = useStateContext();
+  const router = useRouter();
+  const [loadingUsers, setLoadingUsers] = useState(false)
+  let users = ls('users') !== null ? ls('users') : []
+
+  useEffect(() => {
+    if(users < 1) {
+      setLoadingUsers(false)
+    }
+    console.log('load users', users)
+  }, [])
+
+  console.log('declared users', users)
+  const selectUser = () => {
+    ls('activeUID', id)
+    router.push('/')
+  }
+  const showUsers = () => {
+    if(loadingUsers) {
+      return users.map((user) => {
+        return(
+          <div className="login-user__user-box">
+            <img className="login-user__user-img" src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg" />
+            <div className="login-user__user-name">{globalState.test}</div>
+          </div>
+        )
+      })
+    }
+  }
 
   return (
       <div className="login-user">
@@ -15,10 +46,7 @@ const Login = () => {
         </div>
 
         <div className="login-user__form">
-          <div className="login-user__user-box">
-            <img className="login-user__user-img" src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg" />
-            <div className="login-user__user-name">{globalState.test}</div>
-          </div>
+          {showUsers()}
         </div>
         <div className="login-user__buttons">
           <button className="login-user__adult">Add Adult</button>
